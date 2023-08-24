06b0e9f2bea7514a94713a72b456fc30b8f33da0
Ramonsantosss <103340294+Ramonsantossss@users.noreply.github.com>
2023-08-24 13:31:23 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import React from "react";
+import Routes from "./routes";
+//import GlobalStyle from "./global";
+import { ThemeProvider } from "styled-components";
+//import { useStore } from "./store";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [apiData, setApiData] = useState({
-    recentData: [],
-    top1Data: [],
-    top2Data: []
-  });
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const recentResponse = await axios.get('https://ruby-careful-skunk.cyclic.app/recents');
-        const top1Response = await axios.get('https://ruby-careful-skunk.cyclic.app/top/1');
-        const top2Response = await axios.get('https://ruby-careful-skunk.cyclic.app/top/2');
-        
-        setApiData({
-          recentData: recentResponse.data,
-          top1Data: top1Response.data,
-          top2Data: top2Response.data
-        });
-      } catch (error) {
-        console.error('Erro ao buscar dados:', error);
-      }
-    }
-
-    fetchData();
-  }, []);
+  const { state } = useStore();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <div className="api-results">
-        <h2>API Data</h2>
-        <pre>{JSON.stringify(apiData, null, 2)}</pre>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={state.theme}>
+      <Routes />
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
 
---
+++ src/routes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+/*
+import Home from "./pages/Home";
+import manga from "./pages/manga";
+import Watch from "./pages/Watch";
+*/
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
