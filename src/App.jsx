5dc37e28ac7ffa448e417f5ec9cebc9249666931
Ramonsantosss <103340294+Ramonsantossss@users.noreply.github.com>
2023-08-24 13:20:39 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import reactLogo from './assets/react.svg';
-import viteLogo from './vite.svg';
+import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
