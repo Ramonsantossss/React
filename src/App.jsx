4285b103362d709c65a22444c6ca5463bb2a2c35
Ramonsantosss <103340294+Ramonsantossss@users.noreply.github.com>
2023-08-25 14:26:26 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+//import './App.css'
 import styles from './home.module.css';
 
 function App() {
@@ -18,7 +18,7 @@ return (
           const query = event.target[0].value;
 
           if (query.trim().length) {
-            router.push(`/search?q=${query}`);
+            
           }
         }}
       >
