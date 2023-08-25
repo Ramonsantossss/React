710f5ce6fd0caa6bc40bbea8af53da81d17c298c
Ramonsantosss <103340294+Ramonsantossss@users.noreply.github.com>
2023-08-25 14:21:33 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,119 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './styles.css'; // Substitua 'styles.css' pelo caminho correto até o seu arquivo CSS
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import styles from './home.module.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [info, setAnimesRecente] = useState([]);
-  const [data, setAnimesPopular] = useState([]);
-  const [zera, setAnimesTopes] = useState([]);
-
-useEffect(() => {
-async function getMoreRecente() {
-  try {
-    const { data } = await axios.get('https://ruby-careful-skunk.cyclic.app/recents/1');
-    setAnimesRecente(data.slice(0, 15));
-  } catch (err) {
-    console.log("Erro ao obter mais recentes", err);
-  }
-}
-
-async function getMorePopular() {
-  try {
-    const { data } = await axios.get('https://ruby-careful-skunk.cyclic.app/top/1');
-    setAnimesPopular(data.slice(0, 15));
-  } catch (err) {
-    console.log("Erro ao obter mais populares", err);
-  }
-}
-
-async function getMoreTopes() {
-  try {
-    const { data } = await axios.get('https://ruby-careful-skunk.cyclic.app/top/2');
-    setAnimesTopes(data.slice(0, 15));
-  } catch (err) {
-    console.log("Erro ao obter mais topes", err);
-  }
-}
-
-    
-    async function getResultsOfAnimes() {
-      await getMorePopular();
-      await getRecentReleases();
-      await getMoreTopes();
-    }
-
-    getResultsOfAnimes();
-
-}, []);
-
+  const [count, setCount] = useState(0)
 
 return (
-  <div>
-    <form action="/search" method="get">
-      <header className="header-main">
-        {/* Rest of your header code */}
-      </header>
-    </form>
-    <section className="genders">
-      <div className="genders-div">
-        {/* Rest of your gender links */}
-      </div>
-    </section>
-    <main>
-      <section className="section-mangas">
-        <div className="header"># Acabaram de Sair!!!</div>
-        <div className="mangas-container">
-          {setAnimesRecente.map((manga, index) => (
-            <a href="#" key={index}>
-              <div className="manga">
-                <div className="cover"><img src={manga.image} alt={manga.name} /></div>
-                <div className="title">{manga.name}</div>
-                <div className="chapters">Score: {manga.score}</div>
-              </div>
-            </a>
-          ))}
-        </div>
-      </section>
-      <section className="section-mangas">
-        <div className="header"># Recomendações</div>
-        <div className="mangas-container">
-          {setAnimesPopular.map((manga, index) => (
-            <a href="#" key={index}>
-              <div className="manga">
-                <div className="cover"><img src={manga.image} alt={manga.name} /></div>
-                <div className="title">{manga.name}</div>
-                <div className="chapters">Score: {manga.score}</div>
-              </div>
-            </a>
-          ))}
-        </div>
-      </section>
-    </main>
-    <footer>
-      <div className="header">
-        Destaques
-      </div>
-      <section className="mangas-container">
-        {setAnimesTopes.map((manga, index) => (
-          <a href="#" key={index}>
-            <div className="manga highlight">
-              <div className="cover"><img src={manga.image} alt={manga.name} /></div>
-              <div className="title">{manga.name}</div>
-              <div className="chapters">Score: {manga.score}</div>
-            </div>
-          </a>
-        ))}
-      </section>
-    </footer>
-    <div className="modal-manga">
-      <div className="modal-manga-content">
-      </div>
-    </div>
-  </div>
-);
+    <main className={`${styles.main}`}>
+      <h1 className={`${styles.title}`}>Procurar</h1>
+      <form
+        className={`${styles.form}`}
+        onSubmit={(event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          const query = event.target[0].value;
 
+          if (query.trim().length) {
+            router.push(`/search?q=${query}`);
+          }
+        }}
+      >
+        <input
+          className={`${styles.input}`}
+          type="search"
+          placeholder="Pesquisar anime..."
+        />
+      </form>
+      <h1 className={`${styles.title}`}>Populares</h1>
+
+      <ul className={`${styles.list}`}>
+        {animes?.map((item) => (
+          <Link
+            className={`${styles.link}`}
+            key={item?.id}
+            href={`/anime/${item?.id}`}
+          >
+            <li className="flex flex-col justify-between w-[200px]">
+              <img
+                className={`${styles.image}`}
+                src={`https://cdn.appanimeplus.tk/img/${item?.category_icon}`}
+                alt="Anime Banner"
+              />
+              <p className={`${styles.text}`}>{item?.category_name}</p>
+            </li>
+          </Link>
+        ))}
+      </ul>
+
+    </main>
+  );
 }
 
-export default App;
+export async function getServerSideProps() {
+  const res = await fetch('https://animeland.appanimeplus.tk/videoweb/api.php?action=trendingcategory');
+  const animes = await res.json();
+
+  return { props: { animes } };
+}
+}
+
+export default App
